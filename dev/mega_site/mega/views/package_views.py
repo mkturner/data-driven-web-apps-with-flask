41d179fb2f3fe3229d99605189d44ceb53d10638
Marvin T <contact@marvinkturner.com>
2021-08-16 20:21:11 -0400

    ✨ feat: add page for packages by rank

--- dev/mega_site/mega/views/package_views.py
+++ dev/mega_site/mega/views/package_views.py
@@ -6,7 +6,13 @@ import services.package_services as ps
 blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 
-@blueprint.route('/package/<package_name>')
+@blueprint.route('/packages/<package_name>')
 def package_details(package_name: str):
     # return "Package details for: {}".format(package_name)
     return flask.render_template('packages/details.html', name=package_name)
+
+
+# Only capture if an int
+@blueprint.route('/packages/rank/<int:rank>')
+def popular(rank: int):
+    return flask.render_template('packages/details.html', rank=rank)
